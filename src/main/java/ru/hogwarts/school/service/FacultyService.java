22530b801eb7533965473af95ac0609050e96171
mariyatarasenko88 <tmv1231@yandex.ru>
2023-06-24 14:29:23 +0300

    Homework3.2 is donne

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,49 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class FacultyService {
+    private final Map<Long, Faculty> faculties = new HashMap<>();
+    private long lastId = 0;
+
+    public Faculty createFaclulty(Faculty faculty) {
+        faculty.setId(++lastId);
+        faculties.put(lastId, faculty);
+        return faculty;
+    }
+
+    public Faculty findFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        if (faculties.containsKey(faculty.getId())) {
+            faculties.put(faculty.getId(), faculty);
+            return faculty;
+        }
+        return null;
+    }
+
+    public Faculty deleteFaculty(long id) {
+        return faculties.remove(id);
+    }
+
+    public Collection<Faculty> getAllFaculties() {
+        return faculties.values();
+    }
+
+    public Collection<Faculty> getFacultiesByColor(String color) {
+        return faculties.values()
+                .stream()
+                .filter(faculty -> faculty.getColor().equals(color.toLowerCase(Locale.ROOT)))
+                .collect(Collectors.toList());
+    }
+}
